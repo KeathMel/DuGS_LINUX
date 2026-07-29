a68e668a19bd88c17b89c2937c878274a24d5445
KeathMel <KeathiEmpr@protonmail.com>
2026-07-29 09:33:14 +0200

    Update api.py

--- api.py
+++ api.py
@@ -537,7 +537,7 @@ class Handler(BaseHTTPRequestHandler):
 
 
 def main():
-    host = sys.argv[1] if len(sys.argv) > 1 else "0.0.0.0"
+    host = sys.argv[1] if len(sys.argv) > 1 else "127.0.0.1"
     port = int(sys.argv[2]) if len(sys.argv) > 2 else 5800
     register_all_saved_webhooks()
     server = ThreadingHTTPServer((host, port), Handler)
